909ca286a6d433d31b5a1542c248464a40d6f872
Alexandr Savchuk <alexandrsavchuk.97@gmail.com>
2017-11-14 12:13:50 +0300

    Add project files.

---
+++ Tracer/Tracer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tracer
+{
+    public class Tracer
+    {
+        public bool ParseParams(string paramsStr)
+        {
+            if (paramsStr.Length == 0)
+            {
+                return false;
+            }
+
+
+            return true;
+        }
+    }
+}
