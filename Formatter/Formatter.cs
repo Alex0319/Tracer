735c1f2821a6643af4f4bf21507758087bca95ee
Alexandr Savchuk <alexandrsavchuk.97@gmail.com>
2017-11-15 18:17:19 +0300

    change references between assemblies

--- Formatter/Formatter.cs
+++ Formatter/Formatter.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using Tracer.TraceResultData;
+﻿using Tracer.TraceResultData;
 
 namespace Formatter
 {
--- TracerApp/Program.cs
+++ TracerApp/Program.cs
@@ -1,34 +1,41 @@
 ﻿using System;
 using System.Threading;
-using Tracer;
 using Parser;
+using Tracer;
 
 namespace TracerApp
 {
     class Program
     {
         private static readonly IParser parser = Parser.Parser.Instance;
-          
+        private static readonly ITracer tracer = Tracer.Tracer.Instance;
+        private static readonly Formatter.Formatter formatter = Formatter.Formatter.Instance;
+
         static void Main(string[] args)
         {
             parser.Parse(args);
-            TestClass testClass = new TestClass();
-            Console.WriteLine(testClass.FirstTest());
+            TestClass testClass = new TestClass(tracer);
+            testClass.FirstTest();
+            formatter.Format(tracer.GetTraceResult(), parser.GetArgValue('f'));
             Console.ReadLine();
         }
     }
 
     class TestClass
     {
-        private static readonly ITracer tracer = Tracer.Tracer.Instance;
+        private static ITracer tracer;
 
-        public string FirstTest()
+        public TestClass(ITracer tracer)
+        {
+            TestClass.tracer = tracer;
+        }
+
+        public void FirstTest()
         {
             tracer.StartTrace();
             Thread.Sleep(3);
             SecondTest();
             tracer.StopTrace();
-            return tracer.GetTraceResult().ToString();
         }
 
         private void SecondTest()
