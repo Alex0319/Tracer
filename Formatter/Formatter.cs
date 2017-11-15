c54db82416608d7f27758004dba84a502de18d29
Alexandr Savchuk <alexandrsavchuk.97@gmail.com>
2017-11-15 17:58:08 +0300

    add formatter and plugin for JsonFormatter

---
+++ Formatter/Formatter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using Tracer.TraceResultData;
+
+namespace Formatter
+{
+    public class Formatter
+    {
+        private static Formatter _instance;
+        private static PluginLoader _loader;
+
+        public Formatter()
+        {
+            _loader = new PluginLoader();
+            _loader.LoadPlugins();
+        }
+
+        public static Formatter Instance => _instance ?? (_instance = new Formatter());
+
+        public string GetAvailableFormatters()
+        {
+            return _loader.Plugins.Keys.ToString();
+        }
+
+        public bool Format(TraceResult traceResult, string formatType)
+        {
+            if (_loader.Plugins.ContainsKey(formatType))
+            {
+                _loader.Plugins[formatType].Format(traceResult);
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ Formatter/PluginLoader.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using FormatterInterface;
+
+namespace Formatter
+{
+    internal class PluginLoader
+    {
+        private static Dictionary<string ,ITraceResultFormatter> _plugins;
+
+        internal Dictionary<string,ITraceResultFormatter> Plugins => _plugins;
+
+        internal void LoadPlugins()
+        {
+            _plugins = new Dictionary<string, ITraceResultFormatter>();
+            string folder = AppDomain.CurrentDomain.BaseDirectory;
+            string[] files = Directory.GetFiles(folder, "*.dll");
+
+            foreach (var file in files)
+            {
+                ITraceResultFormatter formatterPlugin = IsPlugin(file);
+                if (formatterPlugin != null)
+                {
+                    _plugins.Add(formatterPlugin.GetFormat(), formatterPlugin);
+                }
+            }
+        }
+
+        private ITraceResultFormatter IsPlugin(byte[] file)
+        {
+            try
+            {
+                Assembly assembly = Assembly.Load(file);
+                foreach (Type type in assembly.GetTypes())
+                {
+                    Type interfaceType = type.GetInterface("ITraceResultFormatter");
+                    if (interfaceType != null)
+                    {
+                        return (ITraceResultFormatter)Activator.CreateInstance(type);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                ToLog(e.Message);
+            }
+            return null;
+        }
+
+        private ITraceResultFormatter IsPlugin(string fileUrl)
+        {
+            try
+            {
+                byte[] b = File.ReadAllBytes(fileUrl);
+                return IsPlugin(b);
+            }
+            catch (Exception e)
+            {
+                ToLog(e.Message);
+            }
+            return null;
+        }
+
+        private void ToLog(string text)
+        {
+            try
+            {
+                string folder = AppDomain.CurrentDomain.BaseDirectory;
+                using (StreamWriter streamWriter = new StreamWriter(folder + @"\Log.txt", true))
+                {
+                    streamWriter.WriteLine(DateTime.Now + " : " + text);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
