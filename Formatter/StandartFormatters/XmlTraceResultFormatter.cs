7d57a9518587bb5e413300f2b3db42069451c94e
Alexandr Savchuk <alexandrsavchuk.97@gmail.com>
2017-11-16 13:28:24 +0300

    add xml formatter and change dll output folders

---
+++ Formatter/StandartFormatters/XmlTraceResultFormatter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using FormatterInterface;
+using Tracer.TraceResultData;
+
+namespace Formatter
+{
+    public class XmlTraceResultFormatter : ITraceResultFormatter
+    {
+        private const string Name = "xml";
+
+        public string GetFormat()
+        {
+            return Name;
+        }
+
+        public string Format(TraceResult traceResult)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- JsonFormatter/Properties/AssemblyInfo.cs
+++ JsonFormatter/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("JsonFormatter")]
+[assembly: AssemblyTitle("JsonTraceResultFormatter")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("JsonFormatter")]
+[assembly: AssemblyProduct("JsonTraceResultFormatter")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
